c5762ae84395dca032c69e99da89b10338deac89
Aashu Sailani <rahulsailani619@gmail.com>
2025-01-23 23:39:39 +0530

    improvements

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -6,6 +6,9 @@ import {
   FaHtml5,
   FaCss3Alt,
   FaGitAlt,
+  FaTwitter,
+  FaGithub,
+  FaLinkedin,
 } from 'react-icons/fa';
 import { GiSuitcase } from 'react-icons/gi';
 import {
@@ -33,20 +36,89 @@ function Portfolio() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-900 select-none">
       <div
-        className={`z-10 text-white flex flex-col items-center max-w-2xl px-6 py-8 sm:py-12 lg:pt-24 mx-auto transition-all duration-700 ${
+        className={`z-10 text-white tracking-wider flex flex-col items-center max-w-3xl px-6 py-8 sm:py-12 lg:pt-24 mx-auto transition-all duration-700 ${
           isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10'
         }`}
       >
-        <header className="mb-6 flex flex-col justify-center items-center text-center w-full">
-          <h1 className="text-4xl md:text-5xl lg:text-6xl mb-5 font-bold text-light-gray">
+        <header className="mb-6 flex flex-col justify-between items-center text-center w-full">
+          {/* name + social links */}
+          <div className='flex w-full justify-between items-center'>
+
+          <h1 className="text-4xl md:text-5xl lg:text-6xl mb-4 font-bold text-light-gray">
             Aashu Sailani
           </h1>
-          <div className="flex items-center justify-center gap-5 p-4 h-8 rounded-full bg-green-500 bg-opacity-25 mb-2">
-            <GiSuitcase className="text-2xl animate-blink text-green-200" />
-            <p className="text-base md:text-lg lg:text-xl italic text-green-400 opacity-75">
-              Actively looking for new roles
-            </p>
+          {/* Social links */}
+          <div className='flex gap-5 mb-4'>
+
+            {/* Github */}
+            <div
+              onClick={() => window.open('https://github.com/aashusailani-source', '_blank')}
+              className="cursor-pointer">
+              <span className="cursor-pointer [&>svg]:h-8 [&>svg]:w-8 [&>svg]:fill-[rgb(100,100,100)] hover:[&>svg]:fill-[rgb(255,235,235)] [&>svg]:transition [&>svg]:duration-200 [&>svg]:ease-in-out">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">
+                  <path
+                    d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z" />
+                </svg>
+              </span>
+            </div>
+
+            {/* linkedin */}
+            <div 
+              onClick={() => window.open('https://www.linkedin.com/in/aashu-sailani-5325b633b/', '_blank')}
+              className="cursor-pointer" >
+              <span className="cursor-pointer [&>svg]:h-8 [&>svg]:w-8 [&>svg]:fill-[rgb(100,100,100)] hover:[&>svg]:fill-[rgb(255,255,235)] [&>svg]:transition [&>svg]:duration-200 [&>svg]:ease-in-out">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                  <path
+                    d="M100.3 448H7.4V148.9h92.9zM53.8 108.1C24.1 108.1 0 83.5 0 53.8a53.8 53.8 0 0 1 107.6 0c0 29.7-24.1 54.3-53.8 54.3zM447.9 448h-92.7V302.4c0-34.7-.7-79.2-48.3-79.2-48.3 0-55.7 37.7-55.7 76.7V448h-92.8V148.9h89.1v40.8h1.3c12.4-23.5 42.7-48.3 87.9-48.3 94 0 111.3 61.9 111.3 142.3V448z" />
+                </svg>
+              </span>
+            </div>
+
+            {/* X */}
+            <div
+              onClick={() => window.open('https://x.com/Rahul10287848', '_blank')}
+              className="cursor-pointer" >
+              <span className="cursor-pointer [&>svg]:h-8 [&>svg]:w-8 [&>svg]:fill-[rgb(100,100,100)] hover:[&>svg]:fill-[rgb(255,255,235)] [&>svg]:transition [&>svg]:duration-200 [&>svg]:ease-in-out">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 512 512"
+                >
+                  <path
+                    d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"
+                  />
+                </svg>
+              </span>
+            </div>
+
           </div>
+
+          </div>
+
+          <div className='w-full flex'>
+            <div className="flex items-center justify-center gap-5 p-4 h-8 rounded-lg bg-green-400 bg-opacity-15 mb-8">
+              <GiSuitcase className="text-2xl animate-blink text-green-200" />
+              <p className="text-base md:text-lg lg:text-xl italic text-green-400 opacity-75">
+                Actively looking for new roles
+              </p>
+            </div>
+            
+            <div
+              onClick={() => window.open('/resume.pdf', '_blank')}
+              className="flex items-center justify-center cursor-pointer bg-gradient-to-r from-blue-500 to-blue-700 text-white font-medium rounded-lg p-4 shadow-lg hover:shadow-xl hover:scale-105 transition-transform duration-300">
+              <span>Download resume</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+                className="w-6 h-6 mx-2">
+                <path d="M12 16l-4-4h3V4h2v8h3l-4 4zm-8 2v2h16v-2H4z" />
+              </svg>
+            </div>
+
+
+          </div>
+
           <div className="flex flex-row justify-between items-start w-full mt-4">
             <p className="text-base md:text-lg lg:text-xl text-gray-400">
               Profession
@@ -67,14 +139,15 @@ function Portfolio() {
                 Computer Science and Engineering
               </p>
             </div>
+
           </div>
-          <p className="text-lg text-justify text-light-gray mt-4 tracking-tight">
+          <p className="text-lg text-justify text-light-gray mt-4">
             I’m a Full Stack Developer with expertise in both frontend and
             backend technologies. I specialize in building dynamic,
             user-friendly, and efficient web applications, ensuring seamless
             performance across all layers of development.
           </p>
-          <p className="text-lg text-justify text-light-gray mt-4 tracking-tight">
+          <p className="text-lg text-justify text-light-gray mt-4">
             While my current focus is on full-stack development, I am eager to
             explore new areas like Web3, DevOps, and Cloud Computing in the
             future to broaden my knowledge and skills.
@@ -114,25 +187,25 @@ function Portfolio() {
             {projects.map((project) => (
               <div
                 key={project.id}
-                className="project-item p-6 rounded-lg shadow-lg w-full text-center backdrop-blur-lg bg-opacity-50 border border-white/20"
+                className="project-item p-6 rounded-lg shadow-lg w-full text-start backdrop-blur-lg bg-opacity-50 border border-white/20"
               >
                 <img
                   src={project.image}
                   alt={`${project.title} Screenshot`}
                   className="rounded-md mb-4 w-full h-40 object-fill hover:scale-105 hover:shadow-lg transform transition-all duration-500 cursor-pointer"
                 />
-                <p className="text-xl text-light-gray font-semibold mb-2">
+                <p className="text-2xl text-light-gray font-semibold mb-2">
                   {project.title}
                 </p>
-                <p className="text-light-gray mb-4 text-center tracking-tighter">
+                <p className="text-light-gray mb-4 text-start tracking-wider">
                   {project.description}
                 </p>
-                <div className="flex justify-center gap-4">
+                <div className="flex justify-start items-center gap-4">
                   <a
                     href={project.liveLink}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="px-4 py-2 rounded-lg bg-green-600 text-gray-300 hover:scale-105 hover:bg-green-800 transition-all duration-300"
+                    className="border-2 border-gray-800 rounded-lg px-3 py-2 bg-gray-900 text-gray-200  hover:scale-105 cursor-pointer hover:bg-gray-800 hover:text-gray-200 transform transition-all"
                   >
                     <span className="text transform transition-all duration-500">
                       Live Demo
@@ -145,7 +218,7 @@ function Portfolio() {
                     href={project.githubLink}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="border-2 border-gray-800 rounded-lg px-3 py-2 bg-gray-900 text-gray-200 cursor-pointer hover:bg-gray-800 hover:text-gray-200 transform transition-all"
+                    className="border-2 border-gray-800 rounded-lg px-3 py-2 bg-gray-900 text-gray-200  hover:scale-105 cursor-pointer hover:bg-gray-800 hover:text-gray-200 transform transition-all"
                   >
                     <span className="text transform transition-all duration-500">
                       Source Code
